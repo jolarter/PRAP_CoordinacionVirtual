5a2c83a4d442ed85dff221442abaa60f1f2dd119
Julian Olarte <jolarter@poligran.edu.co>
2015-05-05 16:58:36 -0500

    Se establece la forma final de la página principal.

--- Presentacion/public_html/js/views/message/messageEditView.js
+++ Presentacion/public_html/js/views/message/messageEditView.js
@@ -6,15 +6,10 @@ define([
 ], function($, _, Backbone, messageEditTemplate){
 
   var MessageEditView = Backbone.View.extend({
-    el: $("#container"),
-
     render: function(){
-
-      var that = this;
-	  //..
-	  var data = {};
-      var compiledTemplate = _.template( messageEditTemplate, data );
-      $("#container").html(compiledTemplate);
+      console.log("Se modifica el content");
+      var compiledTemplate = _.template( messageEditTemplate, {} );
+      $("#content").html(compiledTemplate);
     }
 
   });
