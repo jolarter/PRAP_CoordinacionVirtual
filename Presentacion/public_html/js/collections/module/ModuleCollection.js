2d8cce9d9759812178c092f1aac23413e1704c04
DanielSchuler <daniel.andreas87@gmail.com>
2015-05-06 16:46:19 -0500

    nos habia faltado subir las clases de modulos aca estan

---
+++ Presentacion/public_html/js/collections/module/ModuleCollection.js
@@ -0,0 +1,33 @@
+define([
+  'underscore',
+  'backbone',
+], function(_, Backbone, ModuleModel) {
+
+  var ModuleCollection = Backbone.Collection.extend({
+        model: ModuleModel,
+        
+        initialize : function(models, options) {},
+        model: models.MessageModule,
+        
+        url: "http://localhost:8080/Logica/webresources/edu.poli.prap.cv.data.module/",
+        sync: function (method, model, options) {
+            options || (options = {});
+            var errorHandler = {
+                error: function (jqXHR, textStatus, errorThrown) {
+                    // TODO: put your error handling code here
+                    // If you use the JS client from the different domain
+                    // (f.e. locally) then Cross-origin resource sharing 
+                    // headers has to be set on the REST server side.
+                    // Otherwise the JS client has to be copied into the
+                    // some (f.e. the same) Web project on the same domain
+                    alert('Unable to fulfil the request');
+                }}
+            
+            var result = Backbone.sync(method, model, _.extend(options, errorHandler));
+            return result;
+        }
+    });
+
+  return ModuleCollection;
+
+});
