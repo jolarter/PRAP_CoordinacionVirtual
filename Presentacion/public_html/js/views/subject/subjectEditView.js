21dda3ba9e91aed5746e7712acf53456a0fdd46c
daliammosquera <iadla85@hotmail.com>
2015-05-06 15:03:17 -0500

    Se agregan los archvos al proyecto

--- Presentacion/public_html/js/views/subject/subjectEditView.js
+++ Presentacion/public_html/js/views/subject/subjectEditView.js
@@ -6,15 +6,9 @@ define([
 ], function($, _, Backbone, subjectEditTemplate){
 
   var SubjectEditView = Backbone.View.extend({
-    el: $("#container"),
-
     render: function(){
-
-      var that = this;
-	  //..
-	  var data = {};
-      var compiledTemplate = _.template( subjectEditTemplate, data );
-      $("#container").html(compiledTemplate);
+      var compiledTemplate = _.template( subjectEditTemplate, {} );
+      $("#content").html(compiledTemplate);
     }
 
   });
